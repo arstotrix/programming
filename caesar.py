7f3aa42fdb2579617a9269876ca58fa435b32f3c
arstotrix <mashamaslova85@gmail.com>
2017-10-14 22:56:58 +0400

    commit14101

---
+++ caesar.py
@@ -0,0 +1,23 @@
+caesar="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+print("this thing works with cyrillic letters only. sorry about that :D")
+word=input("insert a word: ")
+i=int(input("insert a number from 1 to 32 inclusively: "))
+encr=""
+y=0
+for a in word:
+    for b in caesar:
+        if a==b:
+            encr+=caesar[y+i]
+            y=0
+            break
+        y+=1        
+print(encr)
+if len(word)>=6:
+    pswrd=input("insert password ")
+    if pswrd=="password":
+        print("double encryption enabled!!!")
+        print(encr[3::]+encr[:3])
+        print("поставьте десять плес я так старалась")
+    else:
+        print ("incorrect password, но десять всё равно поставьте")
+
