e7c2a9d6ff8d1c83c7bf66a4027c0660bd007a58
Мария Маслова <mashamaslova85@gmail.com>
2017-12-12 14:12:19 +0300

    Update maslova1.py

--- kontr_re/maslova1.py
+++ kontr_re/maslova1.py
@@ -1,6 +1,6 @@
 #вариант 1
-with open ("Extinct_languages.tsv", encoding="utf-8") as f:
+with open ("Extinct_languages.tsv", encoding = "utf-8") as f:
     lines = f.readlines()
     for line in lines:
-        if(len(line)>35):
+        if(len(line) > 35):
             print(line)
