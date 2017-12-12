97963ec469c8197508153ae4b58aaa11f69efb1a
Мария Маслова <mashamaslova85@gmail.com>
2017-12-12 14:17:26 +0300

    Add files via upload

---
+++ kontr_re/maslova2.py
@@ -0,0 +1,13 @@
+#вариант 1
+i=0;
+with open ("Extinct_languages.tsv", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        pieces = line.split("\t")
+        #print(pieces)
+        if pieces[2] == "Critically endangered\n":
+            i+=1
+            #print(i)
+    print(i)
+        
+            
