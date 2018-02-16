5be10961b4e5c5e0b65e4830dfbc497ebd692e7e
arstotrix <mashamaslova85@gmail.com>
2018-02-16 23:53:54 +0400

    FINALE

--- hw8/mainprog.py
+++ hw8/mainprog.py
@@ -1,3 +1,5 @@
+import random
+dict = {}
 
 def openfile(filename):
     with open (filename, encoding = 'utf-8') as f:
@@ -5,10 +7,49 @@ def openfile(filename):
         words = text.split('\n')
     return words
 
-def main():
-   checkfile = openfile("bear.txt")
-   print(checkfile)
-   
+def dictionarize(text, dict):
+    keyword = text[0]
+    hintlist = []    
+    for i in range (1,len(text)):
+        boom = text[i] + ' ...'
+        hintlist.append(boom)
+    dict[keyword] = hintlist    
+    return dict
+def playgame(dict, n):
+    a = ''
+    for i in range (0, len(n)):
+        print(dict[n][i])
+        t = input('введите версию: ')
+        if (t == n):
+             a = 'победа!'
+             break
+    if a!= 'победа!':
+        a = 'вы проиграли'
+    return a
 
-if '__name__' == '__main__':
+    
+def main():
+    dct = dictionarize(openfile('bear.txt'), dict)
+    dct = dictionarize(openfile('wolf.txt'), dict)
+    dct = dictionarize(openfile('fox.txt'), dict)
+    dct = dictionarize(openfile('hare.txt'), dict)
+    dct = dictionarize(openfile('squirrel.txt'), dict)
+    #print(dct)
+    if random.randint(1,5) == 1:
+        p = playgame (dict, 'медведь')
+        print(p)
+    elif random.randint(1,5) == 2:
+        p = playgame (dict, 'волк')
+        print(p)
+    elif random.randint(1,5) == 3:
+        p = playgame (dict, 'лиса')
+        print(p)
+    elif random.randint(1,5) == 4:
+        p = playgame (dict, 'заяц')
+        print(p)
+    elif random.randint(1,5) == 5:
+        p = playgame (dict, 'белка')
+        print(p)
+    
+if __name__ == '__main__':
     main()
