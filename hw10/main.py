b3b5ffa3166ce87acb4ff6695f468701e5ff64b1
arstotrix <mashamaslova85@gmail.com>
2018-03-30 12:19:42 +0700

    30.03.2

---
+++ hw10/main.py
@@ -0,0 +1,17 @@
+import re
+
+base = []
+
+def openfile(a):
+    with open (a, encoding = "utf-8") as f:
+        text = f.read()
+    return text
+
+def finder(words):
+    
+    return a
+     
+def main():
+   finder(openfile(''))
+if __name__ == "__main__":
+    main()
