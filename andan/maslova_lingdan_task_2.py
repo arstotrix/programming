1b01cfc3e78ad09d8b37d4dc2a175c1f6025d6b0
arstotrix <mashamaslova85@gmail.com>
2018-04-04 21:48:30 +0400

    04.04.05

--- andan/maslova_lingdan_task_2.py
+++ andan/maslova_lingdan_task_2.py
@@ -1,17 +1,17 @@
 def count(n):
     res = 1
-    text = "Извините, вы ввели ноль"
+    text = "Извините, вы ввели не натуральное число." 
     if n > 0:
-        for i in range (1, n+1):
-            res = i **(1/res)
-            #print(res)
-        text = "Член данной последовательности под номером " + str(n) + " равен " + str(res)
+        if int(n) == n:     
+            for i in range (1, int(n)+1):
+                res = i **(1/res)
+        text = "Член данной последовательности под номером " + str(int(n)) + " равен " + str(res)
     return text
 
 #я не знаю, необходима ли эта часть, но пусть будет
     
 def main():
-    print(count(int(input('Введите целое число: '))))
+    print(count(float(input('Введите натуральное число: '))))
     
 if __name__ == '__main__':
     main()
