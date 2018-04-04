221b290e9245caea468c923667f736b56171d0b7
arstotrix <mashamaslova85@gmail.com>
2018-04-04 19:20:43 +0400

    04.04.04

--- andan/maslova_lingdan_task_2.py
+++ andan/maslova_lingdan_task_2.py
@@ -1,11 +1,17 @@
-def count(number):
-    
-    print(res)
+def count(n):
+    res = 1
+    text = "Извините, вы ввели ноль"
+    if n > 0:
+        for i in range (1, n+1):
+            res = i **(1/res)
+            #print(res)
+        text = "Член данной последовательности под номером " + str(n) + " равен " + str(res)
+    return text
 
 #я не знаю, необходима ли эта часть, но пусть будет
     
 def main():
-    count(int(input('Введите число: ')))
+    print(count(int(input('Введите целое число: '))))
     
 if __name__ == '__main__':
     main()
