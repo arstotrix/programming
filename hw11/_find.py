64ac1a043fea5cfc5a7ca321d68a105605c819c2
arstotrix <mashamaslova85@gmail.com>
2018-04-06 20:24:18 +0400

    06.04.02

---
+++ hw11/_find.py
@@ -0,0 +1,17 @@
+import re
+
+def openfile(a):
+    with open (a, encoding = "utf-8") as f:
+        text = f.read()
+    return text
+
+def finder(w):
+    match = re.search('комар\w{0,3}?\W', w).group()
+    print(match)
+     
+def main():
+   replacer((openfile('komar.html')[:1000]))
+if __name__ == "__main__":
+    main()
+
+    
